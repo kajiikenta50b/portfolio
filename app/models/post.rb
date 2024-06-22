a5b1408daf7e035e45ffb8a34de0b9bf5ec3a3e1
梶井健太 <kajiikenta@kajiikentanoMacBook-Air.local>
2024-06-22 16:17:28 +0900

    コメント機能の実装、それに伴うビューファイルの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,14 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
-  validates :image_url, presence: true
+  validates :post_image, presence: true
 
   belongs_to :user
+  has_many :comments, dependent: :destroy
 
   mount_uploader :post_image, PostImageUploader
+
+  def own?(object)
+    id == object&.user_id
+  end
 end
